dd6e6852b7e5bae7482b77c8ce4ce13c0e11dd53
paigni <ikontai4@gmail.com>
2022-10-17 20:25:49 +0600

    Рефакторинг кода

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,6 +1,9 @@
+from django.contrib import admin
 from django.urls import path
-from contact_form import views
+from contact_form.views import ContactCreate, success
 
 urlpatterns = [
-    path("", views.contactform, name="contact"),
+    path('admin/', admin.site.urls),
+    path('', ContactCreate.as_view(), name='contact_page'),
+    path('success/', success, name='success_page')
 ]
