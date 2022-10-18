9f25bcc69f63fda9950f63fc10d9e665b7ac3b98
paigni <ikontai4@gmail.com>
2022-10-18 19:24:09 +0600

    Код в данном файле перенесён в файл приложения

--- project/project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from contact_form.views import ContactCreate, success
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', ContactCreate.as_view(), name='contact_page'),
-    path('success/', success, name='success_page')
-]
